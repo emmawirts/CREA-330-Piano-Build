ad418e0e3fe6e54fa6cd19c7bf903181446c1aff
emmawirts <emmaroselee14@gmail.com>
2025-02-02 20:00:57 -0500

    Added challenge to script.js

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ function playNote(key) {
         key.classList.remove('active');
       });
   }
+
+  const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
+const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j'];
+
+const whiteKeys = document.querySelectorAll('.key.white');
+const blackKeys = document.querySelectorAll('.key.black');
+
+document.addEventListener('keydown', (e) => {
+  // Get the key that triggered the event
+
+    // If the key is being held down, don't play the note again
+    if (e.repeat) {
+        return;
+      }
+      
+  const key = e.key;
+  const whiteKeyIndex = WHITE_KEYS.indexOf(key);
+  const blackKeyIndex = BLACK_KEYS.indexOf(key);
+
+  if (whiteKeyIndex > -1) {
+    playNote(whiteKeys[whiteKeyIndex]);
+  }
+
+  if (blackKeyIndex > -1) {
+    playNote(blackKeys[blackKeyIndex]);
+  }
+});
